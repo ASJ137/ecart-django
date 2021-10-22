2ded1dda20f3828d6c539fa7225365d11bae077b
Abhilash <abhilash.137@hotmail.com>
2021-10-22 17:16:47 +0530

    store and carts functionality

--- carts/urls.py
+++ carts/urls.py
@@ -19,10 +19,12 @@ from . import views
 
 urlpatterns = [
 
-        path('',views.store,name='store'),
-        path('category/<slug:category_slug>/',views.store,name='products_by_category'),
-        path('category/<slug:category_slug>/<slug:product_slug>/',views.product_detail,name='product_detail'),
-        path('search/',views.search,name='search'),
+        path('',views.cart,name='cart'),
+        path('add_cart/<int:product_id>/',views.add_cart,name='add_cart'),
+        path('remove_cart/<int:product_id>/<int:cart_item_id>/',views.remove_cart,name='remove_cart'),
+        path('remove_cart_item/<int:product_id>/<int:cart_item_id>/',views.remove_cart_item,name='remove_cart_item'),
+        #path('<slug:category_slug>/',views.store,name='products_by_category'),
+        #path('<slug:category_slug>/<slug:product_slug>/',views.product_detail,name='product_detail'),
 
 
 ]
