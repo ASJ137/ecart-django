b39335913d650a7ca49b81b6b3071b337a31dbe2
Abhilash Singh <80032953+ASJ137@users.noreply.github.com>
2021-10-23 20:31:37 +0530

    Update settings.py

--- ecart/settings.py
+++ ecart/settings.py
@@ -156,6 +156,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thakur.abhilash1392@gmail.com'
-EMAIL_HOST_PASSWORD = 'Abhilash@123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
